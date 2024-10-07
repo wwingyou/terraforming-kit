1ba29abbf0d66811ebc577aac137d6ec0381484d
wingyou <winwing.you@gmail.com>
2024-10-08 01:51:45 +0900

    feat: fzf bash config

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
+RC="$HOME/.bashrc"
 
 $DIR/install_nvim.sh
 sudo apt-get install fzf
 sudo apt-get install tree
 sudo apt-get install tmux
 sudo apt-get install nnn
+
+cat $DIR/bash_config >> "$RC"
