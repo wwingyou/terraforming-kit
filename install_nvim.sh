888595d3525f43437c23c4d19da1dc9059788001
wingyou <winwing.you@gmail.com>
2024-10-08 00:32:26 +0900

    fix: typo in install_nvim.sh

--- install_nvim.sh
+++ install_nvim.sh
@@ -6,9 +6,9 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 # Configure shell
-RC="~/.bashrc"
+RC="$HOME/.bashrc"
 if [ ! -f "$RC" ]; then
   echo '[INFO] no .bashrc found. create one.'
   touch "$RC"
-end
+fi
 echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> "$RC"
